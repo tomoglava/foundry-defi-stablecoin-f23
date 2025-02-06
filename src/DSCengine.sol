9f8bb98e26e2edfb36a2bafd8ab82eafe6b77df9
tomoglava <tomislav.glavas0@gmail.com>
2025-02-06 22:18:54 +0100

    DSCengine.sol begining

---
+++ src/DSCengine.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.28;
+
+/**
+ * @title DSCEngine
+ * @author tomo
+ * 
+ * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $1 peg.
+ * This stablecon has the properties of being:
+ * - Exogenous Collateral
+ * - Dollar Pegged
+ * - Algoritmically stable
+ * 
+ * It is simmilar to DAI if DAI had no governance, no feesm and was only backed by WETH and WBTC.
+ * 
+ * @notice This contract is the core of the DSC system. It handles all the logic for mining and redeeming DSC, as well as depositing & withdrawing collateral.
+ * @notice This contract is very loosley based on the MakerDAO DSS (DAI) system.
+ *  
+ */
+
+contract DSCEngine {
+
+}
--- src/DecentralizedStablecoin.sol
+++ src/DecentralizedStablecoin.sol
@@ -25,6 +25,12 @@
 
 pragma solidity ^0.8.28;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+// 100% controled by logic (the owner is the DSCEngine)
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 
 /*
 * @title Decentralized Stable Coin
@@ -35,10 +41,44 @@ pragma solidity ^0.8.28;
 *
 * This is the contract meant to be governed by a DSCEngine. This contract is just the ERC20 impelemntation of our stablecoin system.
 */
-contract DecentralizedStableCoin {
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
 
-    constructor() {
-        // constructor
+    error DecentralizedStableCoin__MustBeMoretThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() Ownable(msg.sender) ERC20("DecentralizedStableCoin", "DSC") {
+       //_owner = initialOwner;
+
     }
 
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoretThanZero();
+        }
+
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+
+        //this means use the function from the parent contract (ERC20Burnable)
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+
+        if(_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+
+        if(_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoretThanZero();
+        }
+
+        _mint(_to, _amount);
+
+        return true;
+    }
 }
