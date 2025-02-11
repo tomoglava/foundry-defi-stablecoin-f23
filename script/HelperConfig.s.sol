392f2146d356e03ee49cf7c02847d5a509ef62b7
tomoglava <tomislav.glavas0@gmail.com>
2025-02-11 15:11:07 +0100

    2 tests

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -35,8 +35,7 @@ contract HelperConfig is Script {
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
             weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
             wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
-            //deployerKey: vm.envUint("PRIVATE_KEY")
-            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
+            deployerKey: vm.envUint("PRIVATE_KEY")
         });
     }
 
--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,6 +6,7 @@ import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {Test} from "lib/forge-std/src/Test.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployDSC;
@@ -15,10 +16,17 @@ contract DSCEngineTest is Test {
     address ethUsdPriceFeed;
     address weth;
 
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+
     function setUp() public {
         deployDSC = new DeployDSC();
         (dsc, engine, helperConfig) = deployDSC.run();
         (ethUsdPriceFeed,, weth,,) = helperConfig.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     //////////////////////////
@@ -32,5 +40,20 @@ contract DSCEngineTest is Test {
         uint256 expectedUsdValue = 30000e18;
         uint256 actualUsdValue = engine.getUsdValue(weth, ethAmount);
         assertEq(actualUsdValue, expectedUsdValue);
+    }
+
+    //////////////////////////
+    // Deposit collateral tests     //
+    //////////////////////////
+
+
+    function testRevertIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__MustBeMoretThanZero.selector);
+        engine.depositCollateral(weth, 0);
+        vm.stopPrank();
+
     }
 }
