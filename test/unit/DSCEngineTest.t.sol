b5d7e753af36cccf098d8dff1074034d51355d35
tomoglava <tomislav.glavas0@gmail.com>
2025-02-16 14:40:06 +0100

    started redeem tests

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -16,10 +16,12 @@ contract DSCEngineTest is Test {
     address ethUsdPriceFeed;
     address btcUsdPriceFeed;
     address weth;
+    address wbtc;
 
     address public USER = makeAddr("user");
     uint256 public constant AMOUNT_COLLATERAL = 10 ether;
-    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE_WETH = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE_WBTC = 10 ether;
 
     int256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
     uint256 private constant PRECISION = 1e18;
@@ -29,11 +31,12 @@ contract DSCEngineTest is Test {
     function setUp() public {
         deployDSC = new DeployDSC();
         (dsc, engine, helperConfig) = deployDSC.run();
-        (ethPrice, ethUsdPriceFeed, btcUsdPriceFeed, weth,,) = helperConfig.activeNetworkConfig();
+        (ethPrice, ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc,) = helperConfig.activeNetworkConfig();
 
         ethPrice = ethPrice * ADDITIONAL_FEED_PRECISION;
 
-        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE_WETH);
+        ERC20Mock(wbtc).mint(USER, STARTING_ERC20_BALANCE_WBTC);
     }
 
     //////////////////////////
@@ -110,7 +113,7 @@ contract DSCEngineTest is Test {
         vm.stopPrank();
     }
 
-    modifier depositedCollateral() {
+    modifier depositedCollateral_WETH() {
         vm.startPrank(USER);
         ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
         engine.depositCollateral(weth, AMOUNT_COLLATERAL);
@@ -118,7 +121,15 @@ contract DSCEngineTest is Test {
         _;
     }
 
-    function testCanDepositCollateralAndGetAccountInfo() public depositedCollateral {
+    modifier depositedCollateral_WBTC() {
+        vm.startPrank(USER);
+        ERC20Mock(wbtc).approve(address(engine), AMOUNT_COLLATERAL);
+        engine.depositCollateral(wbtc, AMOUNT_COLLATERAL);
+        vm.stopPrank();
+        _;
+    }
+
+    function testCanDepositCollateralAndGetAccountInfo() public depositedCollateral_WETH {
         (uint256 totalDscMinted, uint256 collateralValueInUsd) = engine.getAccountInformation(USER);
 
         uint256 expectedTotalDscMinted = 0;
@@ -154,13 +165,64 @@ contract DSCEngineTest is Test {
         engine.mintDsc(0);
     }
 
+    // function testRevertIfThereIsNoCollateralEgHealthBroken() public {
+    //     uint256 dscAmount = 1000;
+    //     vm.startPrank(USER);
+
+    //     vm.expectRevert(DSCEngine.DSCEngine__HealthFactorBroken.selector);
+    //     engine.mintDsc(dscAmount);
+
+    //     vm.stopPrank();
+    // }
+
     function testRevertIfThereIsNoCollateralEgHealthBroken() public {
         uint256 dscAmount = 1000;
         vm.startPrank(USER);
 
-        vm.expectRevert();
+        vm.expectRevert(abi.encodeWithSelector(DSCEngine.DSCEngine__HealthFactorBroken.selector, 0));
         engine.mintDsc(dscAmount);
 
         vm.stopPrank();
     }
+
+    //////////////////////////////////
+    // Account information tests    //
+    //////////////////////////////////
+
+    function testGetAccountCollateralValueInUsd() public depositedCollateral_WBTC depositedCollateral_WETH {
+        vm.startPrank(USER);
+        uint256 expectedCollateralValueInUsd =
+            engine.getUsdValue(weth, AMOUNT_COLLATERAL) + engine.getUsdValue(wbtc, AMOUNT_COLLATERAL);
+        vm.stopPrank();
+        uint256 actualCollateralValueInUsd = engine.getAccountCollateralValueInUsd(USER);
+
+        assertEq(expectedCollateralValueInUsd, actualCollateralValueInUsd);
+    }
+
+    function testGetAccountCollateralOfToken() public depositedCollateral_WETH {
+        uint256 actualCollateral = engine.getAccountCollateralOfToken(USER, weth);
+        assertEq(AMOUNT_COLLATERAL, actualCollateral);
+    }
+
+    //////////////////////////////////
+    // Redeem collateral tests      //
+    //////////////////////////////////
+
+    function testRevertIfRedeemAmountZero() public {
+        vm.expectRevert(DSCEngine.DSCEngine__MustBeMoretThanZero.selector);
+        engine.redeemCollateral(weth, 0);
+    }
+
+    //failing because of dividing with unminted Dsc in test health function
+    // function testExpectedUsdValueAfterRedeemSuccess() public depositedCollateral_WETH {
+    //     uint256 redeemAmount = 2 ether;
+
+    //     vm.startPrank(USER);
+    //     uint256 expectedCollateralValueInUsd = engine.getUsdValue(weth, (AMOUNT_COLLATERAL - redeemAmount));
+    //     engine.redeemCollateral(weth, redeemAmount);
+    //     vm.stopPrank();
+
+    //     uint256 actualCollateralValueInUsd = engine.getAccountCollateralValueInUsd(USER);
+    //     assertEq(expectedCollateralValueInUsd, actualCollateralValueInUsd);
+    // }
 }
