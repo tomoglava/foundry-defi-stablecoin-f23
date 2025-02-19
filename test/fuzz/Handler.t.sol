a6cbcc5e1eddcb04ee5b511c85445115af514403
tomoglava <tomislav.glavas0@gmail.com>
2025-02-19 22:03:08 +0100

    first invariant OK

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -1,1 +1,7 @@
+//SPDX-License-Identifier: MIT
+
 // narrow down way we call functions
+
+pragma solidity ^0.8.28;
+
+contract Handler {}
--- test/fuzz/InvariantsTest.t.sol
+++
@@ -1,5 +0,0 @@
-// have our invariants aka properties
-
-//What are our invarionts?
-// 1. The total supply of DSC should allways be less than total value of collateral
-// 2. Getter view should never revert <- evergreen invariant
