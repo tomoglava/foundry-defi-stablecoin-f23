15a7909eaf7da3dd706c5cf1138dd4e31a133ffb
tomoglava <tomislav.glavas0@gmail.com>
2025-02-27 13:49:04 +0100

    end of lesson!

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -11,30 +11,26 @@ pragma solidity ^0.8.28;
  * So if Chainlink Oracle is down, the system will freeze and lot of money stays locked in protocol.
  * @dev Library for getting oracle price
  */
-
-
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-
 library OracleLib {
-
     error OracleLib__StalePrice();
 
     uint256 private constant TIMEOUT = 3 hours; // 3 * 60 * 60 seconds
 
-    function staleCheckLatestRoundData(AggregatorV3Interface priceFeed) 
-    public 
-    view
-    returns (uint80, int256, uint256, uint256, uint80) {
-        
-        (uint80 roundID, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = priceFeed.latestRoundData();
+    function staleCheckLatestRoundData(AggregatorV3Interface priceFeed)
+        public
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        (uint80 roundID, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+            priceFeed.latestRoundData();
 
         uint256 secondsSinceUpdate = block.timestamp - updatedAt;
-        if(secondsSinceUpdate > TIMEOUT) {
+        if (secondsSinceUpdate > TIMEOUT) {
             revert OracleLib__StalePrice();
         }
 
         return (roundID, answer, startedAt, updatedAt, answeredInRound);
-        
     }
 }
